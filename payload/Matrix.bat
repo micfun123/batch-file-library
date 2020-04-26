64345b46c72751a74d72e9bc586419fc989aef5a
micfun123 <54761507+micfun123@users.noreply.github.com>
2020-04-26 10:35:56 +0400

    Add files via upload

---
+++ payload/Matrix.bat
@@ -0,0 +1,5 @@
+@echo off
+color a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto:top
