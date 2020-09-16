22aef8fa8f271a0a53bd8497e9a5775705a00f2f
micfun123 <54761507+micfun123@users.noreply.github.com>
2020-09-16 10:09:33 +0400

    added loading.bat

---
+++ payload/loading.bat
@@ -0,0 +1,35 @@
+@echo off
+cls 
+echo Loading[.      ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[..     ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[...    ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[....   ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[.....  ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[...... ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[.......]
+ping localhost -n 2 >nul
+cls
